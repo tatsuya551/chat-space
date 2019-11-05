cff4401e29675f7bb86d058f3ca2429469db9e20
tatsuya551 <seki0515ya@gmail.com>
2019-11-05 17:08:10 +0900

    Automatic Updating

--- app/assets/javascripts/message.js
+++ app/assets/javascripts/message.js
@@ -1,11 +1,11 @@
 $(function(){
 
-  function buildMessage(message){
+  function buildHTML(message){
     var addImage = message.image? `<img class="lower-message__image" src="${message.image}">`: ` ` 
-    var html = `<div class="message">
+    var html = `<div class="message" data-message_id="${message.id}">
                   <div class="upper-message">
                     <div class="upper-message__user-name">
-                      ${message.name}
+                      ${message.user_name}
                     </div>
                     <div class="upper-message__date">
                       ${message.created_at}
@@ -13,7 +13,7 @@ $(function(){
                   </div>
                   <div class="lower-message">
                     <p class="lower-message__content">
-                      ${message.text}
+                      ${message.content}
                     </p>
                     ${addImage} 
                   </div>
@@ -33,7 +33,7 @@ $(function(){
       contentType: false
     })
     .done(function(message){
-      var html = buildMessage(message);
+      var html = buildHTML(message);
       $('.messages').append(html);
       $('.messages').animate({scrollTop: $('.messages')[0].scrollHeight}, 'fast');
       $('.form__box__submit').prop("disabled", false);
@@ -43,5 +43,29 @@ $(function(){
       alert("メッセージ送信に失敗しました");
       $('.form__box__submit').prop("disabled", false);
     });
-  })
+  });
+  var interval = setInterval(function() {
+    if (window.location.href.match(/\/groups\/\d+\/messages/)){
+      var last_message_id = $('.message:last').data("message_id");
+      $.ajax({
+        url: 'api/messages',
+        type: 'GET',
+        dataType: 'json',
+        data: {id: last_message_id}
+      })
+      .done(function(messages) {
+        var insertHTML = '';
+        messages.forEach(function (message) {
+          insertHTML = buildHTML(message);
+          $('.messages').append(insertHTML);
+        })
+        $('.messages').animate({scrollTop: $('.messages')[0].scrollHeight}, 'fast');
+      })
+      .fail(function() {
+        console.log('error');
+      });
+    } else {
+        clearInterval(interval);
+      }
+  } , 5000 );
 })
--- app/views/messages/create.json.jbuilder
+++ app/views/messages/create.json.jbuilder
@@ -1,4 +1,5 @@
-json.name  current_user.name
-json.created_at  @message.created_at.strftime("%Y/%m/%d %H:%M")
-json.text @message.content
+json.content @message.content
 json.image @message.image.url
+json.created_at @message.created_at.strftime("%Y/%m/%d %H:%M")
+json.user_name @message.user.name
+json.id @message.id
